5bb184ee42b4a5afb34d91524082682fea2b5631
Reinhardbett <bettkiptoo46@gmail.com>
2024-01-14 01:43:28 -0800

    Removed key __class__ during kwargs instantiation

--- models/base_model.py
+++ models/base_model.py
@@ -20,12 +20,14 @@ class BaseModel:
         from models import storage
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
-                if key == "created_at":
+                if key == "__class__":
+                    pass
+                elif key == "created_at":
                     self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                      kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "updated_at":
                     self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                      kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = kwargs[key]
         else:
