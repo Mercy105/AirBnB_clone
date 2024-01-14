f511180bfc91e601f85f984a925fee56a6cc7fb3
Mercy105 <wawerumercy105@gmail.com>
2024-01-13 22:13:47 -0800

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -6,52 +6,44 @@ from datetime import datetime
 
 
 class BaseModel:
-
-    """Class from which all other classes will inherit"""
-
-    def __init__(self, *args, **kwargs):
-        """Initializes public instance attributes
-
-        Args:
-            - *args: list of arguments
-            - **kwargs: dict of key-values arguments
-        """
-
-        from models import storage
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-
-    def __str__(self):
-        """Returns official string representation"""
-
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+    def __init__(self):
+        self.id = str (uuid.uuid())
+        self.created_at = datetime.utcnow()
+        self.created_at = datetime.utcnow()
 
     def save(self):
-        """updates the public instance attribute updated_at"""
+        """
 
-        from models import storage
-        self.updated_at = datetime.now()
-        storage.save()
+        """
+        save.updated_at = datetime.utcnow()
 
-    def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__"""
+    def to_dect(self):
+        """
 
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        return my_dict
+        """
+        inst_dict = self.__dect__.copy()
+        inst_dict["__class__"]-self.__class__.__name__
+        inst_dict["created_at"] = self.created_at.isoformart()
+        inst_dict["updated_at"] = self.updated_at.isoformat()
+
+        return inst_dict
+    
+    def __str__(self):
+        """
+
+        """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
